058e9bb74a2b041c08c05566f8c25c878e046a41
Icy Avocado <dai@ovacoda.com>
2026-09-04 12:36:26 -0600

    feat(tui): wire debugger controls

--- src/frontend/tui/main.c
+++ src/frontend/tui/main.c
@@ -14,14 +14,25 @@ int main(int argc, char **argv) {
   gb_t *gb = gb_create();
   if (gb_load_rom(gb, rom, (size_t)size)) return 1;
   free(rom);
-  initscr(); cbreak(); noecho(); keypad(stdscr, TRUE);
-  gb_regs_t regs; int running = 1;
+  initscr(); cbreak(); noecho(); nodelay(stdscr, TRUE); keypad(stdscr, TRUE);
+  gb_regs_t regs; char disasm[64]; int running = 1, debug = 0, breakpoint = -1;
   while (running) {
-    gb_dbg_regs(gb, &regs); erase();
-    mvprintw(0, 0, "Game Boy TUI  s:step  f:frame  q:quit");
+    gb_dbg_regs(gb, &regs);
+    gb_dbg_disasm(gb, regs.pc, disasm, sizeof disasm);
+    erase();
+    mvprintw(0, 0, "Game Boy TUI  s:step  c:continue  b:break  f:frame  q:quit");
     mvprintw(2, 0, "AF %04X  BC %04X  DE %04X  HL %04X  SP %04X  PC %04X", regs.af, regs.bc, regs.de, regs.hl, regs.sp, regs.pc);
-    refresh(); int key = getch();
-    if (key == 'q') running = 0; else if (key == 's') gb_dbg_step(gb); else if (key == 'f') gb_run_frame(gb);
+    mvprintw(3, 0, "Next: %s%s", disasm, breakpoint >= 0 ? "  [breakpoint]" : "");
+    refresh();
+    int key = getch();
+    if (key == 'q') running = 0;
+    else if (key == 's') gb_dbg_step(gb);
+    else if (key == 'c') { gb_dbg_enable(gb, true); gb_dbg_run_until_break(gb); }
+    else if (key == 'b') {
+      if (breakpoint < 0)
+        breakpoint = gb_dbg_add_bp(gb, (gb_bp_t){GB_BP_PC, regs.pc});
+      else { gb_dbg_del_bp(gb, breakpoint); breakpoint = -1; }
+    } else if (key == 'f') gb_run_frame(gb);
   }
   endwin(); gb_destroy(gb); return 0;
 }
